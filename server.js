db57aa66d714a42ea0419de7ee37cf18271e0d71
iamakeem <bolarinwa_akeem@yahoo.com>
2022-08-17 21:32:48 +0100

    nodemon added

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ let express = require('express');
 
 let app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', function (req, res) {
-  res.send(`
-<!DOCTYPE html>
+  res.send(`<!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
@@ -17,9 +18,9 @@ app.get('/', function (req, res) {
     <h1 class="display-4 text-center py-1">Meekah To-Do App</h1>
     
     <div class="jumbotron p-3 shadow-sm">
-      <form action='/create-item'>
+      <form action="/create-item" method="POST">
         <div class="d-flex align-items-center">
-          <input name='recivedData' autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+          <input name="receivedData" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
           <button class="btn btn-primary">Add New Item</button>
         </div>
       </form>
@@ -56,9 +57,9 @@ app.get('/', function (req, res) {
 `);
 });
 
-app.post('/create-item', function(req, res){
-  console.log("Make this Dynamic")
-  res.send("Thank You!")
-})
+app.post('/create-item', function (req, res) {
+  console.log(req.body.receivedData);
+  res.send('Thank You!');
+});
 
 app.listen(3000);
